45a19555c135c0d8e7b4b2750d336eaf6dfb7860
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-07-15 03:03:03 +0000

    mc

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];  
     let result = 0;
-    try {result != lab.runLab(3);
+    try {result = lab.runLab(3);
       console.log(`${lab.student} code worked: ${result === 27}`);
-    } catch(err) {
-      result = ("Error thrown");
+    } catch(Error) {
+      console.log("Error thrown");
     }
   }
 };
